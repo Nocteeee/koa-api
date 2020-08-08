96543757187d253b7da31bd0ee5e01f0c84dbece
liunian <965205737@qq.com>
2020-08-08 17:18:41 +0800

    feat : deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,23 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
+  apps: [{
+    script: 'app.js',
+    watch: './'
   }, {
     script: './service-worker/',
     watch: ['./service-worker']
   }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+  deploy: {
+    production: {
+      user: 'root',
+      host: '101.133.167.101',
+      ref: 'origin/master',
+      repo: 'https://github.com/Nocteeee/koa-api.git',
+      path: '/website/koa-api',
+      'post-deploy': 'yarn install && pm2 reload ecosystem.config.js --env production',
+      "env": {
+        "NODE_ENV": "production"
+      }
     }
   }
 };
